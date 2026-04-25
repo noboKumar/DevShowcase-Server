4be9387bdd2d0b35f1e1596fb66d66c4cb373656
noboKumar <nobokumar227@gmail.com>
2026-04-26 03:30:32 +0600

    FIX: post projects data issue  and  remove unique from projects

---
+++ prisma/migrations/20260425211600_remove/migration.sql
@@ -0,0 +1,2 @@
+-- DropIndex
+DROP INDEX "projects_userId_key";
--- src/modules/projects/projects.service.ts
+++ src/modules/projects/projects.service.ts
@@ -2,26 +2,9 @@ import { prisma } from "../../lib/prisma";
 import { project } from "../../types";
 
 const addProject = async (payload: project) => {
-  const {
-    title,
-    userId,
-    description,
-    category,
-    githubRepo,
-    liveLink,
-    techStack,
-    thumbnail,
-  } = payload;
   const result = await prisma.projects.create({
     data: {
-      title,
-      userId,
-      description,
-      category,
-      techStack,
-      thumbnail,
-      githubRepo,
-      liveLink,
+      ...payload,
     },
   });
   return result;
